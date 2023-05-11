e31b4f75d9df484a56ec4e852e54e621a6d247e5
Fran6ixneymar <ifebukofrancis@gmail.com>
2023-05-11 16:31:14 -0700

    PYTHON -2

--- 0x02-python-import_modules/4-hidden_discovery.py
+++ 0x02-python-import_modules/4-hidden_discovery.py
@@ -1,6 +1,10 @@
 #!/usr/bin/python3
+
 if __name__ == "__main__":
+    """Display names defined by hidden_4 module."""
     import hidden_4
-    for fig in dir(hidden_4):
+
+    names = dir(hidden_4)
+    for fig in names:
         if fig[:2] != "__":
-            print("{:fig}".format(fig))
+            print(fig)
